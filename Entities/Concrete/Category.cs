10c0b2a1951d4cf725accdb5e7613aca6d7aa8d2
Melikeakg <79082564+Melikeakg@users.noreply.github.com>
2021-02-23 23:16:26 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category1:IEntity
+    {
+        //Çıplak Class Kalmasın
+        public int CategoryId { get; set; }
+        public string CatgeoryName { get; set; }
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product :IEntity
+        // Normalde IEntity'i tanımıyor. Başka katmandaki klasörü kullandık.
+        //public diğer katmanların ulaşması için
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
